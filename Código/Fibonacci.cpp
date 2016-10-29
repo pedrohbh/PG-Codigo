5b90b38aa7b8897777b835a4cf3e91a9fb06a693
Eu <hsufhiusfdhisdfuhisfudhisfud@fdofsdhoshgjhashdoiahdoiadshoh.com>
2016-10-29 18:19:27 -0200

    Decimo Primeiro Commit

--- Código/Fibonacci.cpp
+++ Código/Fibonacci.cpp
@@ -14,6 +14,49 @@ bool FibonacciNode::adicionaFilho( Node *node )
 	return true;
 }
 
+bool FibonacciNode::adicionaIrmao( Node *node )
+{
+	Node *temp = rightMostSibling();
+	if ( temp == NULL )
+		return false;
+	
+	temp->direitaPtr = node;
+	node->esquerdaPtr = temp;
+	node->paiPtr = this->paiPtr;
+	node->direitaPtr = NULL;
+
+	if ( paiPtr )
+		paiPtr->rank++;
+	
+	return true;
+}
+
+bool FibonacciNode::remove()
+{
+	if ( paiPtr )
+	{
+		paiPtr->rank--;
+		if ( esquerdaPtr )
+			paiPtr->filhoPtr = esquerdaPtr;
+		else if ( direitaPtr )
+			paiPtr->filhoPtr = direitaPtr;
+		else
+			paiPtr->filhoPtr = NULL;
+	}
+
+	if ( esquerdaPtr )
+		esquerdaPtr->direitaPtr = direitaPtr;
+	if ( direitaPtr )
+		direitaPtr->esquerdaPtr = esquerdaPtr;
+
+	esquerdaPtr = NULL;
+	direitaPtr = NULL;
+	paiPtr = NULL;
+
+	return true;
+}
+	
+
 bool HeapFibonacci::insereVertice( FibonacciNode *node )
 {
 	if ( node == NULL )
@@ -29,4 +72,18 @@ bool HeapFibonacci::insereVertice( FibonacciNode *node )
 			minRoot = node;
 	}
 	return true;
+}
+
+void HeapFibonacci::decreaseKey( int delta, Node *vertice )
+{
+	vertice->distancia = delta;
+
+	if ( vertice->paiPtr != NULL )
+	{
+		verticePtr->remove();
+		minRoot->adicionaIrmao( vertice );
+	}
+	
+	if ( vertice->distancia < minRoot->distancia )
+		minRoot = vertice;
 }
